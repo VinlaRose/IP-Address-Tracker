9ba8c706810d32e8dcbed6f73ab117486a1a1f94
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2023-02-10 00:13:33 +0530

    initial commit

---
+++ trial2/src/App.js
@@ -0,0 +1,10 @@
+import Home from "./LandingPage/Home";
+
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
---
+++ trial2/src/LandingPage/Home.jsx
@@ -0,0 +1,13 @@
+import pic from "../patternbg.jpg";
+import "./home.css";
+
+function Home() {
+  return (
+    <div className="Top">
+      
+      <img src={pic} className="bg" alt="" />
+    </div>
+  );
+}
+
+export default Home;
