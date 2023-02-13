7b591c40d30ac581e8e5e04328ee8dbc6928f8c4
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2023-02-13 23:43:07 +0530

    hj

--- trial2/src/LandingPage/Home.jsx
+++ trial2/src/LandingPage/Home.jsx
@@ -1,8 +1,13 @@
 import pic from "../patternbg.jpg";
 import "./home.css";
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+
 
 function Home() {
+  
+  
   return (
+    <div>
     <div className="container">
       <img src={pic} className="bg" alt="" />
       <div className="heading">IP Adress Tracker</div>
@@ -40,7 +45,22 @@ function Home() {
         </div>
         
       </div>
+     
     </div>
+     <div id="map">
+     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  {/* <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker> */}
+</MapContainer>
+     </div>
+     </div>
   );
 }
 
