afc82a66663d1865265e17f0b105e79ed4da9f3b
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2023-03-03 16:14:48 +0530

    new api updated

--- trial2/src/LandingPage/Home.jsx
+++ trial2/src/LandingPage/Home.jsx
@@ -3,25 +3,39 @@ import pic from "../patternbg.jpg";
 import "./home.css";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
 import { useState, useEffect } from "react";
+import icon from "./icon";
 
 
 function Home() {
   const [address,setAddress] = useState(null);
   const [ipAddress,setIpAddress] = useState("");
 
-  useEffect(() => {
-    try{
-      const getInitialData = async () => {
-        const res = await fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_h5UqOB1ikdPNHXgXoCZukP78y4JeD&ipAddress=8.8.8.8');
-        const data = await res.json();
-        setAddress(data);
-      }
-      getInitialData();
+ 
+useEffect(() => {
+  try {
+    const getInitialData = async () => {
+      const myHeaders = new Headers();
+      myHeaders.append("apikey", "3umY4AyoFvz9sleRNLewjoFLW0TrZmNT");
+
+      const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
+
+      const ipAddress = "8.8.8.8"; // replace with the IP address you want to look up
+      const url = `https://api.apilayer.com/ip_to_location/${ipAddress}`;
+
+      const res = await fetch(url, requestOptions);
+      const data = await res.json();
+      setAddress(data);
     }
-    catch(error){
-      console.trace(error);
-    }
-  },[])
+    getInitialData();
+  } catch (error) {
+    console.trace(error);
+  }
+}, []);
+
   
   return (
     <div className="entireContainer">
@@ -35,55 +49,50 @@ function Home() {
 
       </div>
 
-      
-      
-   
-
-
       <div className="outputBox" style={{zIndex: 10000}}>
         <div className="subOutputs">
           <p className="smallheading">IP ADDRESS</p>
-          <div className="output">{address.ip}</div>
+          <div className="output">{address && address.ip}</div>
+          {/* <div className="output">8.8.8.8</div> */}
         </div>
         <div class="vl"></div>
         <div className="subOutputs">
           <p className="smallheading">LOCATION</p>
-          <div className="output">{address.location.region}, {address.location.city}, {address.location.country}</div>
+          <div className="output">{address && address.region_name}, {address && address.city}, {address && address.country_code}</div>
+          {/* <div className="output">Manhatten, US</div> */}
         </div>
         <div class="vl"></div>
         <div className="subOutputs">
           <p className="smallheading">TIMEZONE</p>
-          <div className="output">UTC {address.location.timezone}</div>
+          <div className="output">UTC {address && address.timezone}</div>
+          {/* <div className="output">UTC 8:00</div> */}
         </div>
         <div class="vl"></div>
         <div className="subOutputs">
-          <p className="smallheading">ISP</p>
-          <div className="output">{address.isp}</div>
+          <p className="smallheading">CURRENCY</p>
+          <div className="output">{address && address.currencies[0].code}</div>
+          {/* <div className="output">Skylink</div> */}
         </div>
         
       </div>
      
     </div>
-     
 
-    <div>
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}
-    style = {{height:"500px", width:"100%"}}>
+    
+     
+    <div className="map">
+<MapContainer center={[50, -39]} zoom={13} scrollWheelZoom={true} style = {{height:"500px", width:"100%"}}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={[51.505, -0.09]}>
+    <Marker icon={icon} position={[50, -39]}>
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
     </Marker>
   </MapContainer>
     </div>
-    
-  
-  
-
     
      </div>
   );
