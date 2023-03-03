a16c0f7c3c3a3640cd5b1f61ac4687cd8a09de31
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2023-03-03 09:30:39 +0530

    commit

--- trial2/src/LandingPage/Home.jsx
+++ trial2/src/LandingPage/Home.jsx
@@ -1,13 +1,30 @@
+import "/node_modules/leaflet/dist/leaflet.css";
 import pic from "../patternbg.jpg";
 import "./home.css";
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import { useState, useEffect } from "react";
 
 
 function Home() {
-  
+  const [address,setAddress] = useState(null);
+  const [ipAddress,setIpAddress] = useState("");
+
+  useEffect(() => {
+    try{
+      const getInitialData = async () => {
+        const res = await fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_h5UqOB1ikdPNHXgXoCZukP78y4JeD&ipAddress=8.8.8.8');
+        const data = await res.json();
+        setAddress(data);
+      }
+      getInitialData();
+    }
+    catch(error){
+      console.trace(error);
+    }
+  },[])
   
   return (
-    <div>
+    <div className="entireContainer">
     <div className="container">
       <img src={pic} className="bg" alt="" />
       <div className="heading">IP Adress Tracker</div>
@@ -23,43 +40,51 @@ function Home() {
    
 
 
-      <div className="outputBox">
+      <div className="outputBox" style={{zIndex: 10000}}>
         <div className="subOutputs">
           <p className="smallheading">IP ADDRESS</p>
-          <div className="output">192.145.175.123</div>
+          <div className="output">{address.ip}</div>
         </div>
         <div class="vl"></div>
         <div className="subOutputs">
           <p className="smallheading">LOCATION</p>
-          <div className="output">Brooklyun, New York, 100012</div>
+          <div className="output">{address.location.region}, {address.location.city}, {address.location.country}</div>
         </div>
         <div class="vl"></div>
         <div className="subOutputs">
           <p className="smallheading">TIMEZONE</p>
-          <div className="output">UTC - 5:00</div>
+          <div className="output">UTC {address.location.timezone}</div>
         </div>
         <div class="vl"></div>
         <div className="subOutputs">
           <p className="smallheading">ISP</p>
-          <div className="output">SpaceX Starlink</div>
+          <div className="output">{address.isp}</div>
         </div>
         
       </div>
      
     </div>
-     <div id="map">
-     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  {/* <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker> */}
-</MapContainer>
-     </div>
+     
+
+    <div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}
+    style = {{height:"500px", width:"100%"}}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[51.505, -0.09]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+    </div>
+    
+  
+  
+
+    
      </div>
   );
 }
